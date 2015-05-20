7bfeab1bd0f909e723f1f83328aa6dbf35a60445
Максим Шевченко <crazy.unborn@gmail.com>
2015-05-20 03:09:04 +0300

    config commit

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,9 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'MyCars.ru',
+    'language'=>'ru',
+    'theme'=>'mycars',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +22,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'123123123',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		
 	),
 
 	// application components
---
+++ themes/mycars/views/site/index.php
@@ -0,0 +1,8 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Добро пожаловать на сайт <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<p>Тут вы можете подобрать автомобиль по самым низким ценам.</p>
